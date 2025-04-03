9294a9236ac70d15e351bae07590f9a7f29c391a
Soham Majumdar <soham100@hotmail.com>
2025-04-04 04:29:38 +0530

    started frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,45 +1,14 @@
-
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
-import './App.css'
-import LandingPage from './pages/LandingPage'
-import { useSelector } from 'react-redux'
-import LoginPage from './pages/LoginPage';
-import Dashboard from './pages/Dashboard';
-import SignupPage from './pages/SignupPage';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import { useEffect } from 'react';
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import AppRoutes from './routes';
+import './App.css';
 
 function App() {
-
-  // useEffect(
-
-  // ())
-  const user = useSelector(state => state.user);
-  //console.log(user);
-  const ProtectedRoutes = ({children}) => {
-    if(user.userid == undefined) return <Navigate to="/login" />;
-    else return children;
-  }
-
   return (
-    <>
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/dashboard" element={
-            <ProtectedRoutes>
-              <Dashboard />
-            </ProtectedRoutes>
-          } />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/signup" element={<SignupPage />} />
-        </Routes>
-        <Footer />
-      </BrowserRouter>
-    </>
-  )
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
